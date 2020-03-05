4d428c2e76ebd8250feea4e45518b51ab8401d72
Monika Leszczynska <leszczynska.monikaa@gmail.com>
2020-03-05 13:44:22 +0100

    feat: css decoration

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,10 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+})
 
 
